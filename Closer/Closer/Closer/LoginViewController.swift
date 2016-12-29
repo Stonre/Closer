e88782f31b6b064ddac296319e50300bc24b5a99
KamiBruin <wangkaiming617@gmail.com>
2016-12-29 00:28:22 -0800

    Project launching

---
+++ Closer/Closer/Closer/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Closer
+//
+//  Created by Kami on 2016/12/25.
+//  Copyright © 2016年 Kami. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func touchLogin(sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Discover", bundle: nil)
+        let controller = storyboard.instantiateInitialViewController() as UIViewController?
+        
+        if let ctrl = controller {
+            self.present(ctrl, animated: true, completion: nil)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
