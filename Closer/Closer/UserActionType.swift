33ae7a579adf2a38ad06d7d5355e1ed982cee0b5
Stonre <leiding007@gmail.com>
2017-01-07 14:07:57 -0800

    Separate protocol and class to different files; Add virtual fortune property to personal user

---
+++ Closer/Closer/UserActionType.swift
@@ -0,0 +1,17 @@
+//
+//  UserActionType.swift
+//  Closer
+//
+//  Created by Lei Ding on 1/7/17.
+//  Copyright © 2017 Lei Ding. All rights reserved.
+//
+
+import Foundation
+
+enum UserActionType {
+    case DailyFirstUse
+    case ReleaseActivity
+    case ParticipatedInActivity
+    case CompleteActivity
+    case Chat
+}
---
+++ Closer/Closer/VirtualFortune.swift
@@ -0,0 +1,16 @@
+//
+//  VirtualFortune.swift
+//  Closer
+//
+//  Created by Lei Ding on 1/7/17.
+//  Copyright © 2017 Lei Ding. All rights reserved.
+//
+
+import Foundation
+
+
+class VirtualFortune {
+    var quantity = 0
+    
+    func getVirtualFortuneByDailyFirstUse(userAction: UserActionType) {}
+}
