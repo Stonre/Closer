644c7b89bef55338b688dfa345da2172df477192
KamiBruin <wangkaiming617@gmail.com>
2017-02-04 18:14:00 -0800

    Showing activities on MapView

---
+++ Closer/Closer/ActivityMapView/ActivityMKAnnotationView.swift
@@ -0,0 +1,27 @@
+//
+//  ActivityMKAnnotationView.swift
+//  Closer
+//
+//  Created by Kami on 2017/2/4.
+//  Copyright © 2017年 Lei Ding. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ActivityMKPointAnnotation: MKPointAnnotation {
+    
+    var activityDescription: String?
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+//extension ActivityMKAnnotationView: 
+
